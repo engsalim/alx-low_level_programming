7281372d025e14a636386f02628bf2fb4c41dfd0
engsalim <salimatme@gmail.com>
2022-04-08 00:40:55 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main -entry point 
+ *
+ *Return: 0 afgter printing the function 
+ */
+int main (void)
+{
+puts("\"Proramming is like building a multilingual puzzle");
+return (0);
+}
