62e39bca5b5a67ff7ba92fb3aa29bc31631db79a
engsalim <salimatme@gmail.com>
2022-04-09 11:49:13 -0700

    task 7 new updated

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stio.h>
+
+/**
+ * main - Prints the alphabet at reverse
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z''; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
