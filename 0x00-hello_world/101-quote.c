0e78bfff1b9b54ce21ccb4b20706c0b36c5d512a
engsalim <salimatme@gmail.com>
2022-04-10 00:03:18 -0700

    task11 new updated2 hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,9 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	char n[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19";
+
+	fprintf(stderr, "%\n", n);
+
 	return (1);
 }
