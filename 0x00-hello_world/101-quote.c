f1519520d12c8b41aaea0bfb15d12f961e4bce40
engsalim <salimatme@gmail.com>
2022-04-10 00:25:38 -0700

    task11 new3 update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Engtry point
+ *
+ * Return: return 1 (Error)
+ */
+
+int main(void)
+{
+	fprintf(
+			stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	       );
+	return (1);
+}
