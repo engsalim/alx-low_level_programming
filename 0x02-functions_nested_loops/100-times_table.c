ed81f14d9f034df434bd3f9de8a41267964be37b
engsalim <salimatme@gmail.com>
2022-04-12 12:11:16 -0700

    task 12 update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,7 +35,7 @@ void print_times_table(int n)
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar((prod / 10 + '0');
+					_putchar((prod / 10) + '0');
 							}
 							_putchar((prod % 10) + '0');
 							}
