4a1d49ecb767637891de338ae15ad0772397b39f
engsalim <salimatme@gmail.com>
2022-04-12 11:43:41 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table starting with 0
+ *
+ *
+ */
+vlid times_table(void)
+{
+	int i, j, mul;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			mul = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (mul >= 10)
+			{
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
+			}
+			else if ((mul < 10) && (j != 0))
+			{
+				_putchar(' ');
+				_putchar((mul % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
