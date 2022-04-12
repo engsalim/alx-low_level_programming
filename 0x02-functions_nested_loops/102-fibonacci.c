bed7699a5a3201025a9e9218b69a55e5557a0959
engsalim <salimatme@gmail.com>
2022-04-12 12:25:46 -0700

    task 14 update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,14 +2,15 @@
 
 /**
  * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
- * 	separated by a comma followed by a space.
+ * separated by a comma followed by a space.
  *
- * 	Return: Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
 	int count;
 	unsigned long f1 = 0, f2 = 1, sum;
+
 	for (count = 0; count < 50; count++)
 	{
 		sum = f1 + f2;
