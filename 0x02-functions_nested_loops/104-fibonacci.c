81470251b06df2d85fad25caa51992a26587254b
engsalim <salimatme@gmail.com>
2022-04-12 12:54:58 -0700

    task 16 updated1

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 	int count;
-	unsigned long fb1 = 0, fb2 =1, sum;
-	unsigned long fb1_half1,fb1_half2,fb2_half1, fb2_half2;
+	unsigned long fb1 = 0, fb2 = 1, sum;
+	unsigned long fb1_half1, fb1_half2, fb2_half1, fb2_half2;
 	unsigned long half1, half2;
 
-	for (count =0; count <92; count++)
+	for (count = 0; count < 92; count++)
 	{
 		sum = fb1 + fb2;
 		printf("%lu, ", sum);
@@ -27,7 +27,7 @@ int main(void)
 	fb1_half2 = fb1 % 10000000000;
 	fb2_half2 = fb2 % 10000000000;
 
-	for (count =93; count < 99; count++)
+	for (count = 93; count < 99; count++)
 	{
 		half1 = fb1_half1 + fb2_half1;
 		half2 = fb1_half2 + fb2_half2;
