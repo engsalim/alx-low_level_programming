5e95a8138db55f98f1baa2910a5f8833ceba4e54
engsalim <39193592+engsalim@users.noreply.github.com>
2022-05-10 09:13:14 +0300

    Add files via upload

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _ABS_LIKE_MACRO_
+#define _ABS_LIKE_MACRO_
+
+#define ABS(a) (a < 0 ? -(a) : a)
+
+#endif /* _ABS_LIKE_MACRO_ */
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _SUM_LIKE_MACRO_
+#define _SUM_LIKE_MACRO_
+
+#define SUM(a, b) ((a) + (b))
+
+#endif /* _SUM_LIKE_MACRO_ */
